1e350a2a11fee0418eb5fe6f3777531de6b536db
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-09-18 17:53:25 +0700

    feat: Xử lí từ điển 01 (Lọc những từ đơn, chỉ để lại từ ghép)

---
+++ src/XuLiTuDien01/XuLiTuDien01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuLiTuDien01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"C:\git\TuDienTiengViet\ResultXuLiTuDien\vv30K.index";
+            var resultFileName = @"C:\git\TuDienTiengViet\ResultXuLiTuDien\TuDienXuLi01.txt";
+            string[] filelines = File.ReadAllLines(filename);
+            List<string> fileRes = new List<string>();
+            foreach (var item in filelines)
+            {
+                var idx = item.IndexOf("\t");
+                var strConCat = item.Substring(0, idx);
+                //Console.WriteLine(strConCat);
+                if (strConCat.Contains(" "))
+                {
+                    //Console.WriteLine(strConCat);
+                    fileRes.Add(strConCat);
+                }
+            }
+            File.WriteAllLines(resultFileName, fileRes);
+        }
+    }
+}
